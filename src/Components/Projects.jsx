2f7b987549e1c82472f0a33423e16939aa85bfd8
Wilson <fcsie55@gmail.com>
2022-08-09 17:09:23 +0800

    added projects icon

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -12,13 +12,28 @@ function Projects() {
       title: "SpaceX Rockets",
       description:
         "Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae  elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum.",
-    },
-    {
-      image: visualAlgo,
-      alt: "Visual Algo",
-      title: "Visual Algo",
-      description:
-        "Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae  elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      stack: [
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+          alt: "Typescript",
+        },
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+          alt: "React",
+        },
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg",
+          alt: "GraphQL",
+        },
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+          alt: "TailwindCSS",
+        },
+      ],
     },
     {
       image: portfolio,
@@ -26,13 +41,75 @@ function Projects() {
       title: "Portfolio Site",
       description:
         "Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae  elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      stack: [
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+          alt: "Javascript",
+        },
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+          alt: "React",
+        },
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+          alt: "Sass",
+        },
+      ],
     },
     {
       image: visualAlgo,
+      alt: "Visual Algo",
+      title: "Visual Algo",
+      description:
+        "Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae  elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      stack: [
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+          alt: "Javascript",
+        },
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+          alt: "React",
+        },
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+          alt: "CSS3",
+        },
+      ],
+    },
+    {
+      image: null,
       alt: "Kohonen Neural Network",
       title: "Image optimizer with SOM neural network",
       description:
         "Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae  elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      stack: [
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+          alt: "Python",
+        },
+      ],
+    },
+    {
+      image: null,
+      alt: "Kohonen Neural Network",
+      title: "Diffie Hellman - El Gamal Asymmetric Cryptosystem",
+      description:
+        "Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum. Nulla vitae  elit libero, a pharetra augue mollis interdum. Nulla vitae elit libero, a pharetra augue mollis interdum.",
+      stack: [
+        {
+          imgLink:
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+          alt: "Python",
+        },
+      ],
     },
   ];
 
@@ -44,7 +121,7 @@ function Projects() {
         </h1>
         {projectMapper.map((ele) => (
           <Card key={ele.title} classStyle={"project-container"}>
-            <img src={ele.image} alt={`${ele.alt}`} />
+            {ele.image && <img src={ele.image} alt={`${ele.alt}`} />}
             <div className="description">
               <div className="title">
                 <h3>{ele.title}</h3>
