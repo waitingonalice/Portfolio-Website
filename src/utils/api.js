7661c8094d9820902d90a8b1861480a8764dae2e
Wilson <fcsie55@gmail.com>
2022-10-09 19:07:23 +0800

    chore: removed heroku proxy server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,6 @@
 const token = process.env.GATSBY_BOT_TOKEN;
 const chat_id = process.env.GATSBY_USER_ID;
-
 const endpoint = `https://api.telegram.org/bot${token}/sendMessage`;
-const endpointEnv =
-  process.env.NODE_ENV === "development"
-    ? endpoint
-    : `https://cors-anywhere.herokuapp.com/${endpoint}`;
 
 export const sendTelegramMessage = async (text) => {
   const unescapeMessage = {
@@ -13,7 +8,7 @@ export const sendTelegramMessage = async (text) => {
     message: text.message.replace(/(\r\n|\n|\r)/g, " "),
   };
   try {
-    const response = await fetch(endpointEnv, {
+    const response = await fetch(endpoint, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
