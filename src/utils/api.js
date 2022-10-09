40ece501084bcbe02ecdbd88a3dfce2f6fe89969
Wilson <fcsie55@gmail.com>
2022-10-09 18:15:24 +0800

    chore: added more access control into headers

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,6 +13,10 @@ export const sendTelegramMessage = async (text) => {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+        "Access-Control-Allow-Methods":
+          "GET, POST, PATCH, PUT, DELETE, OPTIONS",
       },
       body: JSON.stringify({ chat_id, text: unescapeMessage }, null, 2),
     });
